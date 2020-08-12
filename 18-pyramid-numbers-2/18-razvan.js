ce8f34570d7dbf6f2bb7f1acd4b4a8dcb9964d1d
Podar Razvan <podarrazvan@pop-os.localdomain>
2020-08-12 19:07:26 +0300

    18 pyramid numbers 2

---
+++ 18-pyramid-numbers-2/18-razvan.js
@@ -0,0 +1,44 @@
+
+const inp = 7;
+
+function pyramid(levels) {
+  
+  let linesNumber = levels - 1;
+  let level = "";
+  const line = " ";
+  let number = "";
+  for(let i = 0 ; i < levels; i ++) {
+    if(i === 0){
+      number = "1"
+      start = 2;
+    }else{
+      number = constructNumbers ((i+1) *2,start);
+      start = number[1];
+      
+    }
+    level = line.repeat(linesNumber) + number[0] + line.repeat(linesNumber);
+    
+    linesNumber --; // scade in mod egal numarul de spatii
+    console.log(level);
+  }
+}
+
+function constructNumbers(nrOfElements,start) {
+  let nr = "";
+  let countElement = 1;
+  let nrToAdd = start;
+  while(true) {
+    if(countElement === nrOfElements) {
+      break
+    }
+    if(nrToAdd === 10) {
+      nrToAdd = 0;
+    }
+    nr += nrToAdd.toString();
+    nrToAdd++;
+    countElement ++;
+ }
+  return [nr, nrToAdd];
+}
+
+ pyramid(inp);
