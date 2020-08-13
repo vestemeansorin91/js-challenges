a07e2b4e586596c1dc927f5742120b59ef3aeffe
Podar Razvan <podarrazvan@pop-os.localdomain>
2020-08-13 16:52:10 +0300

    03 which is bigger

---
+++ 03-which-is-bigger/03-razvan.js
@@ -0,0 +1,13 @@
+// Create a function that takes two numbers (2 paramaters) and return a message with "The biggest number is " + number.
+
+// INPUT 5 12
+// OUTPUT 'The biggest number is 12'
+
+const nr1 = 5;
+const nr2 = 12;
+
+if(nr1 > nr2) {
+  alert(`The biggest number is ${nr1}`);
+}else {
+    alert(`The biggest number is ${nr2}`);
+}
