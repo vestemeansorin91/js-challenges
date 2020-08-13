db80f3586dd3ca3f8bc26c92524a4e05fd01179c
Podar Razvan <podarrazvan@pop-os.localdomain>
2020-08-13 16:42:18 +0300

    01 odd or even

---
+++ 01-odd-or-even/01-razvan.js
@@ -0,0 +1,8 @@
+// Create a function that takes a number and returns a string that says if the given number is 'odd' or 'even'
+
+// INPUT 19
+// OUTPUT 'odd'
+
+let input = 19;
+
+input % 2 === 0 ? alert('Even') : alert('Odd');
