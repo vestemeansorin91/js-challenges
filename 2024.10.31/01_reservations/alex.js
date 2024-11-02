d661c24991c4cdb9701d0156ac9d61a804974ca8
mihaifirst <varzaru.alexmihai@gmail.com>
2024-11-02 18:23:08 +0200

    updateReservation refactored

---
+++ 2024.10.31/01_reservations/alex.js
@@ -0,0 +1,34 @@
+const reservations = [];
+const id = reservations.length + 1;
+
+function createReservation(date, startHour, endHour, range) {
+  reservations.push({
+    id,
+    date,
+    startHour,
+    endHour,
+    range,
+    categories: [],
+  });
+  return reservations;
+}
+createReservation("2024-10-31", 6, 9, 15);
+
+function updateReservationDate(id, updateReservation) {
+  let reservation = reservations.find((res) => {
+    return res.id === id;
+  });
+  if (!reservation) {
+    return null;
+  }
+
+  for (let i = 0; i < Object.keys(updateReservation).length; i++) {
+    const key = Object.keys(updateReservation)[i];
+    if (reservation.hasOwnProperty(key)) {
+      reservation[key] = updateReservation[key];
+    }
+  }
+
+  return reservation;
+}
+let updatedReserv = updateReservationDate(1, { startHour: 2, endHour: 10 });
