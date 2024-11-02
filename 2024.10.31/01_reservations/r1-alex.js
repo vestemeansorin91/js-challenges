356db90e6972f661f399e759b515b11bc2938b6c
mihaifirst <varzaru.alexmihai@gmail.com>
2024-11-02 11:11:12 +0200

    r2-problem

--- 2024.10.31/01_reservations/r1-alex.js
+++ 2024.10.31/01_reservations/r1-alex.js
@@ -13,3 +13,24 @@ function createReservation(date, startHour, endHour, range) {
   return reservations;
 }
 createReservation("2024-10-31", 6, 9, 15);
+
+function updateReservationDate(id, updateRes) {
+  let reserv = reservations.find((res) => {
+    return res.id === id;
+  });
+  if (!reserv) {
+    return null;
+  }
+
+  const keys = Object.keys(updateRes);
+
+  for (let i = 0; i < keys.length; i++) {
+    const key = keys[i];
+    if (reserv.hasOwnProperty(key)) {
+      reserv[key] = updateRes[key];
+    }
+  }
+
+  return reserv;
+}
+let updatedReserv = updateReservationDate(1, { startHour: 12, endHour: 10 });
