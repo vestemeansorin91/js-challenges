581651e8192929292f77a2544388bfab0f4f1ab2
mihaifirst <varzaru.alexmihai@gmail.com>
2024-11-12 13:16:25 +0200

    cinema functions wip

--- 2024.10.31/02-cinema/services/cinema.service.js
+++ 2024.10.31/02-cinema/services/cinema.service.js
@@ -1,17 +1,39 @@
-import fs from 'fs';
+import fs from "fs";
 
-const { cinemas } = JSON.parse(fs.readFileSync('./data/cinemas.json', 'utf-8'));
+const { cinemas } = JSON.parse(fs.readFileSync("./data/cinemas.json", "utf-8"));
 
 const cinemaService = {
-    findCinemaById: (cinemaId) => { 
-      return cinemas.find(cinema => cinema.cinemaId === cinemaId) || null;
-     },
-    listCinemasByLocation: (location) => { /* implementation */ },
-    getCinemaFacilities: (cinemaId) => { /* implementation */ },
-    getCinemaScreens: (cinemaId) => { /* implementation */ },
-    getAvailableSeats: (cinemaId, movieId, showtime) => { /* implementation */ },
-    bookSeat: (cinemaId, movieId, showtime, seatId) => { /* implementation */ },
-    getCinemaCapacity: (cinemaId) => { /* implementation */ },
-  };
-  
-  export default cinemaService;  
+  findCinemaById: (cinemaId) => {
+    return cinemas.find((cinema) => cinema.cinemaId === cinemaId) || null;
+  },
+  listCinemasByLocation: (location) => {
+    return cinemas.filter((cinema) => cinema.location === location);
+  },
+  getCinemaFacilities: (cinemaId) => {
+    let selectedCinema = cinemas.find((cinema) => cinema.cinemaId === cinemaId);
+    if (!selectedCinema) {
+      return [];
+    }
+
+    return selectedCinema.facilities;
+  },
+  getCinemaScreens: (cinemaId) => {
+    let selectedCinema = cinemas.find((cinema) => cinema.cinemaId === cinemaId);
+    if (!selectedCinema) {
+      return [];
+    }
+
+    return selectedCinema.screens;
+  },
+  getAvailableSeats: (cinemaId, movieId, showtime) => {
+    /* implementation */
+  },
+  bookSeat: (cinemaId, movieId, showtime, seatId) => {
+    /* implementation */
+  },
+  getCinemaCapacity: (cinemaId) => {
+    /* implementation */
+  },
+};
+
+export default cinemaService;
